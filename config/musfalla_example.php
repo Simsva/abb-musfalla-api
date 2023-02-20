d3503d0e7f1f4980693687dc401bcda34ad0b95e
Simon Ericsson <simon@krlsg.se>
2023-02-20 16:47:04 +0100

    Moved mail notifications to PHPMailer

--- config/musfalla_example.php
+++ config/musfalla_example.php
@@ -6,6 +6,12 @@ return array(
   "user" => "postgres",
   "password" => "p455w0rd",
   "origin" => "*",
-  "mail_sender" => "musfalla@simsva.se",
+  "smtp_auth" => true,
+  "smtp_secure" => "tls",
+  "smtp_host" => "smtp.office365.com",
+  "smtp_port" => 587,
+  "smtp_username" => "mailsender@hotmail.com",
+  "smtp_sender" => "mailsender@hotmail.com", // can be different from username
+  "smtp_password" => "p455w0rd",
 );
 ?>
